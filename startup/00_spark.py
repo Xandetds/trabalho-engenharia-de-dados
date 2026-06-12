3b56e15b989c20627b54fb04d417fc9ed3be8f0a
Bruno Monteiro Bonifácio <brunombonifacio@gmail.com>
2026-06-12 12:51:27 -0300

    fix: remover configuração desnecessária no spark referente à configuração do MinIO, deverá ser adicionada na atividade adequada

--- startup/00_spark.py
+++ startup/00_spark.py
@@ -4,7 +4,6 @@ spark = (
     SparkSession.builder
     .appName('Trabalho_Eng_Dados')
     .master('local[*]')
-    .config('spark.jars.packages', 'io.delta:delta-spark_2.12:3.2.0,org.apache.hadoop:hadoop-aws:3.3.4')
     .config('spark.sql.extensions', 'io.delta.sql.DeltaSparkSessionExtension')
     .config('spark.sql.catalog.spark_catalog', 'org.apache.spark.sql.delta.catalog.DeltaCatalog')
     .getOrCreate()
